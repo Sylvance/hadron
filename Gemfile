0a8aad71c1b216f2b722150a1df6c61cd1643e65
Sylvance <kerandisylvance@gmail.com>
2022-12-08 14:10:27 +0300

    [hadron] Add hadron shell script

--- Gemfile
+++ Gemfile
@@ -5,8 +5,8 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in hadron.gemspec
 gemspec
 
+gem "proto_dsl", "~> 0.1.1"
 gem "rake", "~> 13.0"
-
 gem "rspec", "~> 3.0"
-
 gem "rubocop", "~> 1.21"
+gem "thor", "~> 1.2"
--- lib/hadron.rb
+++ lib/hadron.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
-require_relative "hadron/version"
+Dir[File.join(__dir__, "hadron", "*.rb")].sort.each { |file| require file }
 
 module Hadron
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.create_project(name)
+    puts "Creates project #{name}"
+  end
+
+  def self.create_database
+    puts "Creates database"
+  end
+
+  def self.migrates_database
+    puts "Migrates database"
+  end
+
+  def self.generate_scaffold(name)
+    puts "Generates scaffold #{name}"
+  end
 end
