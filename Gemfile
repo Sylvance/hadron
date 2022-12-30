5f534d916b9db27992c683304e7c2248dae5030e
Sylvance <kerandisylvance@gmail.com>
2022-12-30 13:54:20 +0300

    [hadron] More trials

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ source "https://rubygems.org"
 # Specify your gem's dependencies in hadron.gemspec
 gemspec
 
-gem "proto_dsl", "~> 0.1.1"
+gem "pry", "~> 0.14.1"
 gem "rake", "~> 13.0"
 gem "rspec", "~> 3.0"
 gem "rubocop", "~> 1.21"
--- lib/hadron.rb
+++ lib/hadron.rb
@@ -4,20 +4,4 @@ Dir[File.join(__dir__, "hadron", "*.rb")].sort.each { |file| require file }
 
 module Hadron
   class Error < StandardError; end
-
-  def self.create_project(name)
-    puts "Creates project #{name}"
-  end
-
-  def self.create_database
-    puts "Creates database"
-  end
-
-  def self.migrates_database
-    puts "Migrates database"
-  end
-
-  def self.generate_scaffold(name)
-    puts "Generates scaffold #{name}"
-  end
 end
